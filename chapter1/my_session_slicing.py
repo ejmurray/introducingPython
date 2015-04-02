ddb379d77eacce4a5695f6e8d2425fba244b7ba1
ejmurray <ejmurray247@hotmail.com>
2015-04-02 09:51:22 +0100

    Create my_session_slicing.py

---
+++ chapter1/my_session_slicing.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+letters = 'abcdefghijklmnopqrstuvwxyz'
+letters[:]
+letters[20:]
+letters[10:]
+letters[12:15]
+letters[-3:]
+letters[18:-3]
+letters[-6:-2]
+letters[::7]
+letters[::2]
+letters[:5:2]
+letters[:6:2]
+letters[:7:2]
+letters[4:20:3]
+letters[19::3]
+letters[19::4]
+letters[:21:4]
+letters[:21:5]
+letters[-1::-1]
+letters[::-1]
+letters[-50:]
+letters[-51:-50]
+letters[:70]
+letters[70:71]
+len(letters)
+empty = ''
+len(empty)
