b24c44892ed78836ded2ea06b595c551a298f7a0
ejmurray <ejmurray247@hotmail.com>
2015-04-02 09:50:39 +0100

    Update my_session.py

--- chapter1/my_session.py
+++ chapter1/my_session.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 import sys; print('Python %s on %s' % (sys.version, sys.platform))
-sys.path.extend(['C:\\Users\\ernest\\Documents\\scripts\\introducingPython'])
 letters = 'abcdefghijklmnopqrstuvwxyz'
 print(letters[0])
 letters[1]
